c0797f100727e8628f3e9ae72e0c8425d72e4483
Rafael Yebenes <terenrafa@hotmail.com>
2019-04-10 19:53:33 +0200

    initial commit
    initial commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Site Description Here">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/stack-interface.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/socicon.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/flickity.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/iconsmind.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/jquery.steps.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700%7CMerriweather:300,300i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body >
+    <div class="main-container">
+        <section class="height-100 imagebg text-center" data-overlay="4">
+            <div class="background-image-holder">
+                <img alt="background" src="img/loginAsociacionFondo.png" />
+            </div>
+            <div class="container pos-vertical-center">
+                <div class="row">
+                    <div class="col-md-7 col-lg-5">
+
+                        <form id="formLogin">
+                            <h2>Inicio Sesión Asociaciones</h2>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <input type="text" placeholder="Email"/>
+                                </div>
+                                <div class="col-md-12">
+                                    <input type="password" placeholder="Contraseña" />
+                                </div>
+                                <div class="col-md-6">
+                                    <button class="btn btn--primary type--uppercase" type="submit">Iniciar Sesión</button>
+                                </div>
+                                <div class="col-md-6">
+                                    <button class="btn btn-warning type--uppercase btn-lg" type="button" id="btnRegistro">Crear Cuenta</button>
+                                </div>
+                            </div>
+                            <!--end of row-->
+                            <span class="type--fine-print block">¿Ha olvidado sus datos?
+                                <a href="page-accounts-recover.html">Recuperar cuenta</a>
+                            </span>
+                        </form>
+
+                        <form id="formRegistro">
+                            <h2>Registro Asociación</h2>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <img src="img/baseMan.png" style="border-radius: 50%; border: 7px solid #FEFFFD;" height="150px" width="150px" >
+                                </div>
+
+                                <div class="col-md-6">
+                                    <input type="text" name="name" placeholder="Nombre *">
+                                </div>
+                                <div class="col-md-6">
+                                 <input type="text" name="direccion" placeholder="Dirección *">
+                             </div>
+                             <div class="col-md-6">
+                                <input type="text" name="name" placeholder="Telefono *">
+                            </div>
+                            <div class="col-md-6">
+                             <input type="text" name="direccion" placeholder="Dirección *">
+                         </div>
+                         <div class="col-md-12">
+                            <input type="text" placeholder="Email *"/>
+                            <input type="password" placeholder="Contraseña *" />
+                        </div>
+                    </div>
+
+                </form>
+
+
+            </div>
+        </div>
+        <!--end of row-->
+    </div>
+    <!--end of container-->
+</section>
+</div>
+<!--<div class="loader"></div>-->
+<a class="back-to-top inner-link" href="#start" data-scroll-class="100vh:active">
+    <i class="stack-interface stack-up-open-big"></i>
+</a>
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/flickity.min.js"></script>
+<script src="js/easypiechart.min.js"></script>
+<script src="js/parallax.js"></script>
+<script src="js/typed.min.js"></script>
+<script src="js/datepicker.js"></script>
+<script src="js/isotope.min.js"></script>
+<script src="js/ytplayer.min.js"></script>
+<script src="js/lightbox.min.js"></script>
+<script src="js/granim.min.js"></script>
+<script src="js/jquery.steps.min.js"></script>
+<script src="js/countdown.min.js"></script>
+<script src="js/twitterfetcher.min.js"></script>
+<script src="js/spectragram.min.js"></script>
+<script src="js/smooth-scroll.min.js"></script>
+<script src="js/scripts.js"></script>
+
+<script type="text/javascript">
+    $('#formRegistro').hide();
+    $('#btnRegistro').click(function(){
+        $('#formLogin').hide();
+        $('#formRegistro').show();
+    });
+    $('#btnVolver').click(function(){
+       $('#formLogin').show();
+       $('#formRegistro').hide();
+   });
+
+</script>
+</body>
+</html>
